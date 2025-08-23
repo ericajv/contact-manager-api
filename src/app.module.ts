de97bb399fd4177ecacfef35b06ba388c368039c
ericaj <ericaj.vieira13@gmail.com>
2025-08-23 11:46:43 -0300

    feat: initial commit with project setup

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from 'src/env'
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controllers/authenticate-controller';
+import { CreateContactController } from './controllers/create-contact.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController, 
+    AuthenticateController, 
+    CreateContactController,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
